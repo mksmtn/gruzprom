8b8f4f94a233935c811419a18eb032374293e0b6
Maxim Matyunin <krabovm@gmail.com>
2023-03-03 01:38:00 +0400

    feat(b2b-ui): initial commit

---
+++ packages/b2b-ui/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'gruzprom-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent {
+  title = 'b2b-ui';
+}
