421434f2a440a7c24b4a6799b680908538e8a8f0
Maxim Matyunin <krabovm@gmail.com>
2023-03-16 22:06:21 +0400

    chore(b2b-ui): install ngrx

--- packages/b2b-ui/src/main.ts
+++ packages/b2b-ui/src/main.ts
@@ -3,9 +3,19 @@ import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+
+const rootReducers = {};
+
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes,
+      withEnabledBlockingInitialNavigation()),
+      provideStore(rootReducers),
+      provideEffects([]),
+  ],
 }).catch((err) => console.error(err));
