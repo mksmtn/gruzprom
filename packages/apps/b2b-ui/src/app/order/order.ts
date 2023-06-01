03372a82c56fdae0c188f093d2eea3fda7d0df29
Maxim Matyunin <krabovm@gmail.com>
2023-06-01 21:27:54 +0000

    feat(b2b-ui): add loader

--- packages/apps/b2b-ui/src/app/order/order.ts
+++ packages/apps/b2b-ui/src/app/order/order.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ClarityModule } from '@clr/angular';
-import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
-import { Observable, combineLatest, map } from 'rxjs';
+import { ClarityModule, ClrLoadingState } from '@clr/angular';
+import {
+  FormlyFieldConfig,
+  FormlyFormOptions,
+  FormlyModule,
+} from '@ngx-formly/core';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
 import { TodayService } from '../shared/providers/today.service';
 import { createFormlyFields } from './formly-fields';
 import { CommonModule } from '@angular/common';
@@ -24,18 +28,43 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class OrderComponent {
   protected readonly formGroup = new FormGroup({});
   protected readonly model = {};
+  protected readonly options: FormlyFormOptions = {};
   protected readonly fields$: Observable<FormlyFieldConfig[]> = combineLatest([
     this.todayService.dayChanges(),
   ]).pipe(map(([today]) => createFormlyFields({ today })));
+  protected readonly loading$ = new BehaviorSubject(ClrLoadingState.DEFAULT);
+  protected readonly LoadingState = ClrLoadingState;
+  private isNextChangeDueToReset = false;
 
   constructor(
     private readonly todayService: TodayService,
     private readonly httpClient: HttpClient
   ) {}
 
+  protected onModelChange(): void {
+    if (
+      !this.isNextChangeDueToReset &&
+      this.loading$.value !== ClrLoadingState.DEFAULT
+    ) {
+      this.loading$.next(ClrLoadingState.DEFAULT);
+    }
+    if (this.isNextChangeDueToReset) {
+      this.isNextChangeDueToReset = false;
+    }
+  }
+
   protected onSubmit(): void {
-    this.httpClient
-      .post('/api/orders', { data: this.model })
-      .subscribe({ next: console.log, error: console.error });
+    this.loading$.next(ClrLoadingState.LOADING);
+    this.httpClient.post('/api/orders', { data: this.model }).subscribe({
+      next: () => {
+        this.isNextChangeDueToReset = true;
+        this.options.resetModel?.();
+        this.loading$.next(ClrLoadingState.SUCCESS);
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading$.next(ClrLoadingState.ERROR);
+      },
+    });
   }
 }
